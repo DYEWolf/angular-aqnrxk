451fd7f945f06fab4bfdacde21e8d3cec047282d
DYEWolf <angel.rmarin@outlook.com>
2020-12-14 19:01:40 -0600

    added alerts

--- src/app/students.component.ts
+++ src/app/students.component.ts
@@ -1,5 +1,5 @@
 import { DecimalPipe } from "@angular/common";
-import { Component, QueryList, ViewChildren } from "@angular/core";
+import { Component, QueryList, ViewChildren, ViewChild } from "@angular/core";
 import { Observable } from "rxjs";
 
 import { Student } from "./student";
@@ -10,6 +10,10 @@ import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { ModalComponent } from "./modal.component";
 
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
+import { NgbAlert } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "students",
@@ -23,11 +27,39 @@ export class StudentsComponent {
 
   faTrash = faTrash;
 
+  private _success = new Subject<string>();
+
+  private warning = new Subject<string>();
+
+  staticAlertClosed = false;
+  successMessage = "";
+  deleteMessage = "";
+
   @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
+
+  @ViewChild("selfClosingAlert", { static: false }) selfClosingAlert: NgbAlert;
+
+  @ViewChild("deleteSelfClosingAlert", { static: false }) deleteSelfClosingAlert: NgbAlert;
 
   constructor(public service: StudentsService, public modalService: NgbModal) {
     this.students$ = service.students$;
     this.total$ = service.total$;
+  }
+
+  ngOnInit(): void {
+    this._success.subscribe(message => (this.successMessage = message));
+    this._success.pipe(debounceTime(3000)).subscribe(() => {
+      if (this.selfClosingAlert) {
+        this.selfClosingAlert.close();
+      }
+    });
+
+    this.warning.subscribe(message => (this.deleteMessage = message));
+    this.warning.pipe(debounceTime(3000)).subscribe(() => {
+      if (this.deleteSelfClosingAlert) {
+        this.deleteSelfClosingAlert.close();
+      }
+    });
   }
 
   onSort({ column, direction }: SortEvent) {
@@ -51,8 +83,8 @@ export class StudentsComponent {
       "Are you sure you want to remove this student?"
     );
     if (answer) {
-      console.log("yes");
       this.service.deleteStudent(student);
+      this.warning.next(`Student deleted`);
     }
   }
 
@@ -70,9 +102,14 @@ export class StudentsComponent {
     modalRef.componentInstance.student = student;
     modalRef.result.then(result => {
       if (student) {
-        if (mode === "new") this.service.addStudent(student);
+        if (mode === "new") {
+          this.service.addStudent(student);
+          this._success.next(`Student saved successfully`);
+        }
         if (mode === "edit") this.service.editStudent(student);
       }
     });
   }
+
+
 }
